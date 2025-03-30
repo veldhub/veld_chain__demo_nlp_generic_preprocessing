3facf0d8cfe00d6a42752125e284636ed447326b
SteffRhes <steffrhes@pm.me>
2025-03-30 21:21:53 +0200

    test, veld_preprocess_regex_replace: wip

--- test.sh
+++ test.sh
@@ -24,7 +24,7 @@ file_out=./data/demo_01/out/grimms_fairy_tales__lowercased.txt
 md5_out_correct=7d1efcc7a70e479d71405fe24a84e7ea
 rm $file_out 2> /dev/null
 
-file_out_metadata=./data/demo_01/out/veld__grimms_fairy_tales.yaml
+file_out_metadata=./data/demo_01/out/veld__grimms_fairy_tales__lowercased.yaml
 md5_out_metadata_correct=624fe00777dd3d260b714632afb121b5
 rm $file_out_metadata 2> /dev/null
 
@@ -51,7 +51,7 @@ file_out_dirty=./data/demo_02/out/grimms_fairy_tales__dirty.txt
 md5_out_dirty_correct=074bdd74e5bc95b6023dd7040bb61320
 rm $file_out_dirty 2> /dev/null
 
-file_out_metadata=./data/demo_02/out/veld__grimms_fairy_tales.yaml
+file_out_metadata=./data/demo_02/out/veld__grimms_fairy_tales__clean.yaml
 md5_out_metadata_correct=b70c028a520f5e27d1f76849e7569aaf
 rm $file_out_metadata 2> /dev/null
 
@@ -63,6 +63,29 @@ echo "finished" | tee -a test.log
 compare "$file_in" "$md5_in_correct"
 compare "$file_out_clean" "$md5_out_clean_correct"
 compare "$file_out_dirty" "$md5_out_dirty_correct"
+compare "$file_out_metadata" "$md5_out_metadata_correct"
+
+
+echo "--- test veld_demo_03_regex_replace.yaml ---" | tee test.log
+
+file_in=./data/demo_03/in/grimms_fairy_tales.txt
+md5_in_correct=3b460a560a61b4e70e5ccc6f63c48063
+
+file_out=./data/demo_03/out/grimms_fairy_tales__newline_removed.txt
+md5_out_correct=e696a7fd33f597f1a7f2315fb271e5a1
+rm $file_out_clean 2> /dev/null
+
+file_out_metadata=./data/demo_03/out/veld__grimms_fairy_tales__newline_removed.yaml
+md5_out_metadata_correct=863fb5cfef112bbd9cb209fe612e540b
+rm $file_out_metadata 2> /dev/null
+
+command="docker-compose -f veld_demo_03_regex_replace.yaml up"
+echo "executing: ${command}" | tee -a test.log
+eval "$command" &>> test.log
+echo "finished" | tee -a test.log
+
+compare "$file_in" "$md5_in_correct"
+compare "$file_out" "$md5_out_correct"
 compare "$file_out_metadata" "$md5_out_metadata_correct"
 
 
